eaaa4dbc7ed436d08ad757cc068e761b1aacb3cf
torixmicah <51023135+torixmicah@users.noreply.github.com>
2023-11-11 12:36:31 +1100

    setting up PWA

--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
 // public/script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const fetchButton = document.getElementById('fetchButton');
-    const interpretButton = document.getElementById('interpretButton');
-  
-    fetchButton.addEventListener('click', fetchPassage);
-    interpretButton.addEventListener('click', interpretText);
-  });
-  
-  function fetchPassage() {
-    const passageInput = document.getElementById('passage-input'); // Make sure your input has this ID
+  const fetchButton = document.getElementById('fetchButton');
+  const interpretButton = document.getElementById('interpretButton');
+
+  fetchButton.addEventListener('click', fetchPassage);
+  interpretButton.addEventListener('click', interpretText);
+});
+
+function fetchPassage() {
+  const passageInput = document.getElementById('passage-input'); // Make sure your input has this ID
   const [book, chapter, verse] = passageInput.value.split(/\s+|\:|\./); // Split input value to get book, chapter and verse
   const passageDisplay = document.getElementById('passage-display'); // Display area for the passage
-  
+
   fetch(`https://3cajh2zyfi.ap-southeast-2.awsapprunner.com/fetch-passage?book=${encodeURIComponent(book)}&chapter=${encodeURIComponent(chapter)}&verse=${encodeURIComponent(verse || '')}`)
     .then(response => response.json())
     .then(data => {
@@ -21,19 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error('Error fetching passage:', error);
       passageDisplay.textContent = 'Error fetching passage.';
     });
-  }
-  
-  function interpretText() {
-    const selectedText = getSelectedText();
-    const denomination = getSelectedDenomination();
-    console.log(selectedText)
-    console.log(denomination)
-    
-    fetch('https://3cajh2zyfi.ap-southeast-2.awsapprunner.com/interpret-text', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ text: selectedText, denomination: denomination })
-    })
+}
+
+function interpretText() {
+  const selectedText = getSelectedText();
+  const denomination = getSelectedDenomination();
+  console.log(selectedText)
+  console.log(denomination)
+
+  fetch('https://3cajh2zyfi.ap-southeast-2.awsapprunner.com/interpret-text', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text: selectedText, denomination: denomination })
+  })
     .then(response => response.json())
     .then(data => {
       // Display the interpretation in the designated area
@@ -41,20 +41,51 @@ document.addEventListener('DOMContentLoaded', () => {
       interpretationDisplay.textContent = data.interpretation;
     })
     .catch(error => console.error('Error:', error));
+}
+
+function getSelectedText() {
+  // Use window.getSelection to get the user-selected text on the webpage
+  if (window.getSelection) {
+    return window.getSelection().toString();
+  } else if (document.selection && document.selection.type !== "Control") {
+    return document.selection.createRange().text;
   }
-  
-  function getSelectedText() {
-    // Use window.getSelection to get the user-selected text on the webpage
-    if (window.getSelection) {
-      return window.getSelection().toString();
-    } else if (document.selection && document.selection.type !== "Control") {
-      return document.selection.createRange().text;
+  return ''; // Return empty string if no text is selected
+}
+
+function getSelectedDenomination() {
+  // Assuming you have a select element with id="denomination-select" for the user to choose a denomination
+  const denominationSelect = document.getElementById('denomination-select');
+  return denominationSelect.value; // Return the selected option value
+}
+
+// Installation button for phone
+let deferredPrompt;
+const addToHomeScreenButton = document.getElementById('addToHomeScreen');
+
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to show the Add to Home Screen button
+  addToHomeScreenButton.style.display = 'block';
+});
+
+addToHomeScreenButton.addEventListener('click', () => {
+  // Hide the button as we are about to show the prompt
+  addToHomeScreenButton.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the Add to Home Screen prompt');
+    } else {
+      console.log('User dismissed the Add to Home Screen prompt');
     }
-    return ''; // Return empty string if no text is selected
-  }
-  
-  function getSelectedDenomination() {
-    // Assuming you have a select element with id="denomination-select" for the user to choose a denomination
-    const denominationSelect = document.getElementById('denomination-select');
-    return denominationSelect.value; // Return the selected option value
-  }
+    deferredPrompt = null;
+  });
+});
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/public/style.css',
+  '/public/script.js'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
